bfe2a28e9207c6db20fab51d6f549b6efd73e7c2
Jrb Youssef <jrb.youssef@gmail.com>
2021-06-26 23:28:10 +0100

    Refactor preloader

--- src/preload.js
+++ src/preload.js
@@ -1,27 +1,8 @@
-const { totalmem, freemem, uptime } = require('os')
-const { formatBytes, formatSeconds } = require('./utils/formatters')
+const memPreloader = require('./preloaders/memoryPreloader')
 
 window.addEventListener('DOMContentLoaded', () => {
-  // memory
-  const totalMemDisp = document.getElementById('memory-totalmem')
-  const freeMemoryDisp = document.getElementById('memory-freemem')
-  const uptimeDisp = document.getElementById('memory-uptime')
-
-  setInterval(() => {
-    // totalmem
-    totalMemDisp.innerHTML = ''
-    totalMemDisp.innerHTML = formatBytes(totalmem())
-
-    // freemem
-    freeMemoryDisp.innerHTML = ''
-    freeMemoryDisp.innerHTML = formatBytes(freemem())
-
-    // uptime
-    uptimeDisp.innerHTML = ''
-    uptimeDisp.innerHTML = formatSeconds(uptime)
-
-    // uptime
-  }, 1000)
+  // preload memory
+  memPreloader()
 
 })
 
